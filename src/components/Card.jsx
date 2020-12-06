80bd67218cf9014790faf15d0367b4434ae163df
krsntn <kanahan0110@gmail.com>
2020-12-06 23:13:19 +0800

    change tabs to spaces

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
 const Card = (props) => {
-	const { data } = props;
-	const { name, html_url, description, language } = data;
+  const { data } = props;
+  const { name, html_url, description, language } = data;
 
-	return (
-		<div className="relative m-6 h-72 w-64 bg-gray-200 dark:bg-gray-800 rounded-lg text-sm transform duration-200 hover:-translate-y-2">
-			<div
-				className="rounded-t-lg bg-gray-300 dark:bg-gray-900 flex justify-start items-center"
-				style={{ height: 20 }}
-			>
-				<div className="rounded-full bg-red-700 h-2 w-2 ml-3 mr-1" />
-				<div className="rounded-full bg-yellow-500 h-2 w-2 mr-1" />
-				<div className="rounded-full bg-green-600 h-2 w-2 mr-1" />
-			</div>
-			<div
-				className="p-6 flex flex-col justify-around text-black dark:text-white"
-				style={{ height: 'calc(100% - 20px)' }}
-			>
-				<div className="">{name}</div>
-				<div className="">{description}</div>
-				<div className="">{language}</div>
-				<a href={html_url}>
-					<button className="p-2 px-3 w-full bg-green-500 rounded text-white">
-						Learn More
-					</button>
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="relative m-6 h-72 w-64 bg-gray-200 dark:bg-gray-800 rounded-lg text-sm transform duration-200 hover:-translate-y-2">
+      <div
+        className="rounded-t-lg bg-gray-300 dark:bg-gray-900 flex justify-start items-center"
+        style={{ height: 20 }}
+      >
+        <div className="rounded-full bg-red-700 h-2 w-2 ml-3 mr-1" />
+        <div className="rounded-full bg-yellow-500 h-2 w-2 mr-1" />
+        <div className="rounded-full bg-green-600 h-2 w-2 mr-1" />
+      </div>
+      <div
+        className="p-6 flex flex-col justify-around text-black dark:text-white"
+        style={{ height: 'calc(100% - 20px)' }}
+      >
+        <div className="">{name}</div>
+        <div className="">{description}</div>
+        <div className="">{language}</div>
+        <a href={html_url}>
+          <button className="p-2 px-3 w-full bg-green-500 rounded text-white">
+            Learn More
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/Helmet.jsx
+++ src/components/Helmet.jsx
@@ -2,54 +2,54 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 const HelmetComponent = ({ title }) => {
-	return (
-		<div>
-			<Helmet>
-				{/* <!-- HTML Meta Tags --> */}
-				<title>{title}</title>
-				<meta
-					name="description"
-					content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
-				/>
+  return (
+    <div>
+      <Helmet>
+        {/* <!-- HTML Meta Tags --> */}
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
+        />
 
-				{/* <!-- Google / Search Engine Tags --> */}
-				<meta itemprop="name" content="Reed" />
-				<meta
-					itemprop="description"
-					content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
-				/>
-				<meta
-					itemprop="image"
-					content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
-				/>
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemprop="name" content="Reed" />
+        <meta
+          itemprop="description"
+          content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
+        />
+        <meta
+          itemprop="image"
+          content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
+        />
 
-				{/* <!-- Facebook Meta Tags --> */}
-				<meta property="og:url" content="https://reedbarger.com" />
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content="Reed" />
-				<meta
-					property="og:description"
-					content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
-				/>
-				<meta
-					property="og:image"
-					content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
-				/>
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://reedbarger.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Reed" />
+        <meta
+          property="og:description"
+          content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
+        />
+        <meta
+          property="og:image"
+          content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
+        />
 
-				{/* <!-- Twitter Meta Tags --> */}
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:title" content="Reed" />
-				<meta
-					name="twitter:description"
-					content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
-				/>
-				<meta
-					name="twitter:image"
-					content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
-				/>
-			</Helmet>
-		</div>
-	);
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Reed" />
+        <meta
+          name="twitter:description"
+          content="Articles, tips, and insights aimed at making you a world-class developer, designer, and beyond."
+        />
+        <meta
+          name="twitter:image"
+          content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
+        />
+      </Helmet>
+    </div>
+  );
 };
 
 export default HelmetComponent;
