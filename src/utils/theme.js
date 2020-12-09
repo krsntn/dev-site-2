2c91979f0f405c167393e8e255fad73fb58dbd69
krsntn <kanahan0110@gmail.com>
2020-12-10 01:18:30 +0800

    handle localstroage

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,29 +1,33 @@
 export function isCurrentDarkTheme() {
-  return (
-    localStorage.theme === 'dark' ||
-    (!('theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches)
-  );
+  if (typeof window !== `undefined`) {
+    return (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    );
+  }
 }
 
 export function setNewTheme(theme) {
-  console.log(localStorage.theme, theme);
-  if (theme === 'dark') {
-    localStorage.setItem('theme', theme);
-    document.querySelector('html').classList.add('dark');
-  } else {
-    document.querySelector('html').classList.remove('dark');
-    localStorage.theme = 'light';
+  if (typeof window !== `undefined`) {
+    if (theme === 'dark') {
+      localStorage.setItem('theme', theme);
+      document.querySelector('html').classList.add('dark');
+    } else {
+      document.querySelector('html').classList.remove('dark');
+      localStorage.theme = 'light';
+    }
   }
 }
 
 export function initialLoadTheme() {
-  console.log('localstorage', localStorage.theme);
-  setNewTheme(
-    localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-      ? 'dark'
-      : 'light'
-  );
+  if (typeof window !== `undefined`) {
+    setNewTheme(
+      localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches)
+        ? 'dark'
+        : 'light'
+    );
+  }
 }
